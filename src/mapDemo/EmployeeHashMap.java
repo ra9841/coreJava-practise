48c0a30dde0c31139376468ce9b7d8118b4e2b24
Rabin <rabin.shrestha7@gmail.com>
2023-08-07 11:54:19 -0700

    core java practise

---
+++ src/mapDemo/EmployeeHashMap.java
@@ -0,0 +1,29 @@
+package mapDemo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class EmployeeHashMap {
+
+	public static void main(String[] args) {
+		Map<Integer,String>ma=new HashMap<>();
+		                   ma.put(2,"Rahul");
+		                   ma.put(5,"Anil");
+		                   ma.put(null,"Hari");                 //one null is allowed in key
+		                   ma.put(20,null);                     //multiple null is allowed in value
+		                   ma.put(10, null);
+		                   ma.put(6,"Sabita");
+		                   ma.put(15,"Anil");                   //duplicay is allowed in value
+		System.out.println(ma);  
+		//Iterating the data
+		Set<Map.Entry<Integer,String>>se=ma.entrySet();
+		
+		for(Map.Entry<Integer,String>temp:se) {
+			System.out.println(temp);
+		}
+		
+
+	}
+
+}
